ffbebccf880be0cfd687c346b808790a129763ac
laurentius4619 <83589509+laurentius4619@users.noreply.github.com>
2021-05-06 22:21:06 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import test_module as test
 radius = test.number_input()
 print(test.get_circumference(radius))
 print(test.get_circle_area(radius))
+
--- test_module.py
+++ test_module.py
@@ -9,3 +9,8 @@ def get_circumference(radius):
 
 def get_circle_area(radius):
     return PI * radius * radius
+
+if __name__ == "__main__":
+    print("get_circumference(10):", get_circumference(10))
+    print("get_circle_are(10):", get_circle_area(10))
+
